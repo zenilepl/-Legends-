47548883befbb7d1be1d831791de775cd6a94e98
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-07 00:47:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,7 +172,7 @@ client.on('message', message => {
 
 client.on('message', message => {
  
-    if (message.content === "$bot") {
+    if (message.content === "+bot") {
 var year = message.guild.createdAt.getFullYear()
 var month = message.guild.createdAt.getMonth()
 var day = message.guild.createdAt.getDate()
